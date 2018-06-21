592c0f25aafdf2a0d3a00a3647f1144070030134
vincent <vincent@vincents-iMac.local>
2018-06-21 20:00:40 +0800

    banner finish

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img class="banner-img" src="http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_600x330_f922b488.jpg"/>
+      <div class="banner-info">
+        <div class="banner-title">
+          故宫（AAAAA景区）
+        </div>
+        <div class="banner-number">
+          <span class="iconfont banner-icon">&#xe692;</span>
+          39
+        </div>
+      </div>
+    </div>
+    <common-gallary :imgs="imgs" v-show="showGallary" @close="handleGallaryClose"></common-gallary>
+  </div>
+</template>
+
+<script>
+import CommonGallary from 'common/gallary/Gallary'
+export default {
+  name: 'DetailBanner',
+  components: {
+    CommonGallary
+  },
+  data () {
+    return {
+      showGallary: false,
+      imgs: ['https://img1.qunarzz.com/sight/p0/1806/4f/4f448c94f180b36da3.water.jpg_600x330_1f0ec716.jpg']
+    }
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGallary = true
+    },
+    handleGallaryClose () {
+      this.showGallary = false
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .banner
+    position relative
+    overflow hidden
+    height 0
+    padding-bottom 55%
+    .banner-img
+      width 100%
+    .banner-info
+      display flex
+      position absolute
+      left 0
+      right 0
+      bottom 0
+      line-height .6rem
+      color #ffffff
+      background-image linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
+      .banner-title
+        flex 1
+        font-size .32rem
+        padding 0 .2rem
+      .banner-number
+        margin-top .14rem
+        padding 0 .4rem
+        line-height .4rem
+        height .32rem
+        font-size .24rem
+        border-radius .2rem
+        background rgba(0, 0, 0, .8)
+        .banner-icon
+          font-size .24rem
+</style>
